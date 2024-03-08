8ba82a608d12df819f445131714fb62aa649ace2
omer9706 <omer_yurtseven1997@outlook.com>
2024-03-08 13:45:25 +0300

    About page completed .

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className='text-white text-8xl w-full h-full '>
-      Page
+    <div className='text-white  w-full h-full px-20 flex flex-col gap-10'>
+      <h1 className=' text-6xl font-bold text-green-500'>Hakkımızda</h1>
+      <p className='text-xl lg:pr-96'>
+        Öncelikle siz değerli müşterilerimize Ankara Jeneratör firması olarak hoşgeldiniz diyoruz.
+        Ankara Jeneratör firması olarak jeneratör satış ve kiralama hizmetleri, bakım ve servis işlemleri, yedek parça ve aksesuarlar konusunda uzman kadromuzla hizmetinizdeyiz.
+        Profesyonel kadromuzla sizlere en iyi hizmeti sunmak için buradayız.
+      </p>
+      <Image
+        src={'/about.jpg'}
+        alt='about'
+        width={1920}
+        height={1080}
+        className='object-cover lg:rounded-2xl h-96 lg:h-full'
+      />
     </div>
   )
 }
